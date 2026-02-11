c3ac12a47b654d191a8816879e3e9675ebd2dcaf
dianziji <syang22@tufts.edu>
2026-02-10 23:00:57 -0500

    chore(next): allow wp image hosts for next/image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "newbethelrc.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.newbethelrc.org",
+      },
+      {
+        protocol: "https",
+        hostname: "**.wp.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
